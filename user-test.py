1ae05d3089d40244b4a1581edd56757402da48f5
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-10-23 11:42:56 +0300

    Update user-test.py

--- user-test.py
+++ user-test.py
@@ -9,13 +9,13 @@ class TestUser(unittest.TestCase):
         '''
         A set up method which runs before each test case
         '''
-        self.new_user = User('Fredrick','19xy13!')
+        self.new_user = User('TheCaffeine','19xy13!')
 
     def test_init(self):
         '''
         Test method that checks if the user object is initialised properly
         '''
-        self.assertEqual(self.new_user.name, 'Fredrick')
+        self.assertEqual(self.new_user.name, 'TheCaffeine')
         self.assertEqual(self.new_user.password, '19xy13!')
 
     def test_save_user(self):
