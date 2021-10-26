456cee6f80a1db29a7d987c14f1cd6e3c66c48c5
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-10-26 09:22:57 +0300

    files update

--- user-test.py
+++ user-test.py
@@ -1,20 +1,23 @@
 import unittest
 from user import User
 
+
 class TestUser(unittest.TestCase):
-    '''
+    """
     Test class defining test cases for user  class
-    '''
+    """
+
     def setUp(self):
-        '''
-        A set up method which runs before each test case
-        '''
-        self.new_user = User('TheCaffeine','19xy13!')
+        """
+        A set up # noinspection PyStatementEffect
+method which runs before each test case
+        """
+        self.new_user = User('TheCaffeine', '19xy13!')
 
     def test_init(self):
-        '''
-        Test method that checks if the user object is initialised properly
-        '''
+        """
+        Test method that checks if the user var = object is initialised properly
+        """
         self.assertEqual(self.new_user.name, 'TheCaffeine')
         self.assertEqual(self.new_user.password, '19xy13!')
 
@@ -22,33 +25,34 @@ class TestUser(unittest.TestCase):
         '''
         Test method to test if the user has been saved on the user list
         '''
-        self.new_user.save_user() # method to save user
-        self.assertEqual(len(User.user_list),2) #expecting 1 !=2 since there is only one contact saved currently
+        self.new_user.save_user()  # method to save user
+        self.assertEqual(len(User.user_list), 2)  # expecting 1 !=2 since there is only one contact saved currently
 
     def test_save_many_users(self):
-        '''
+        """
         Test case to check if multiple users can be saved
-        '''
-        self.new_user.save_user() # method to save user
-        test_user = User('test','user','any')
+        """
+        self.new_user.save_user()  # method to save user
+        test_user = User('test', 'user', 'any')
         test_user.save_user()
-        self.assertEqual(len(User.user_list),4)
+        self.assertEqual(len(User.user_list), 4)
 
     def test_display_users(self):
         '''
         test case to check if users are displayed
         '''
-        self.assertEqual(User.display_users(),User.user_list)
+        self.assertEqual(User.display_users(), User.user_list)
+
     def test_user_exists(self):
         '''
         test case to check if the user exists in the user list
         '''
         self.new_user.save_user()
-        test_user = User('test','name','password')
+        test_user = User('test', 'name', 'password')
         test_user.save_user()
         user_exits = User.user_exists('test')
         self.assertTrue(user_exits)
 
+
 if __name__ == '__main__':
     unittest.main()
-    
